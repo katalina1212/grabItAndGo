d3f97262879839b913be8a2df9f2c0d3d2674a68
katalin1212 <7827@ait.nsw.edu.au>
2021-06-28 19:56:00 +1000

    Finished orderActivity layout (Shopping cart) TimePicker Layout is is ready with some functionality

---
+++ app/src/main/java/com/example/grabitandgo/TimePickerActivity.java
@@ -0,0 +1,79 @@
+package com.example.grabitandgo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.TimePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TimePickerActivity extends AppCompatActivity {
+
+    TextView tvTimer1;
+    int t1Hour, t1Minute;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_picker);
+
+        tvTimer1 =findViewById(R.id.tv_timer1);
+
+        tvTimer1.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+
+            public void onClick(View v){
+                //initialize time picker dialog
+                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                        TimePickerActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                //initialize hour and minute
+                                t1Hour = hourOfDay;
+                                t1Minute = minute;
+
+                                //Store hour and minute in string
+                                String time = t1Hour + "=" + t1Minute;
+                                //Initialize 24 hours time format
+                                SimpleDateFormat f24Hours = new SimpleDateFormat(
+                                        "HH:mm"
+                                );
+
+
+                                try {
+                                    Date date = f24Hours.parse(time);
+
+                                    //Initialize 12 hours time format
+                                    SimpleDateFormat f12Hours = new SimpleDateFormat(
+                                            "hh:mm aa"
+                                    );
+                                    tvTimer1.setText(f12Hours.format(date));
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },12,0,false
+                );
+                //Set transparent background
+                timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                //Display previous selected time
+                timePickerDialog.updateTime(t1Hour, t1Minute);
+                //Show dialog
+                timePickerDialog.show();
+            }
+        }
+
+        );
+    }
+}
